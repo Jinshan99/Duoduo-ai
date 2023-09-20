3030b3c27603bb09b41efbe1ce922b24a86918f4
yangjins99 <yangjins@usc.edu>
2023-09-19 23:41:46 -0700

    initial commit

---
+++ app/api/code/route.ts
@@ -0,0 +1,56 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi, chatCompletionRequestMessage } from "openai";
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+// import OpenAI from 'openai';
+const instructionMessage: chatCompletionRequestMessage = {
+    role: "system",
+    content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
+}
+export async function POST(
+    req: Request
+) {
+    try {
+        const { userId } = auth();
+        const body = await req.json();
+        const { messages } = body;
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        // if (!openai.apiKey) {
+        //     return new NextResponse("OpenAI API Key not configured.", { status: 500 });
+        // }
+
+        if (!configuration.apiKey) {
+            return new NextResponse("OpenAI API Key not configured.", { status: 500 });
+        }
+
+        if (!messages) {
+            return new NextResponse("Messages are required", { status: 400 });
+        }
+        const freeTrial = await checkApiLimit();
+        if (!freeTrial) {
+            return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+        }
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [instructionMessage, ...messages]
+        });
+        await incrementApiLimit();
+        // const response = await openai.chat.completions.create({
+        //     model: "gpt-3.5-turbo",
+        //     messages
+        // });
+
+        return NextResponse.json(response.data.choices[0].message);
+
+    } catch (error) {
+        console.log('[CODE_ERROR]', error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
